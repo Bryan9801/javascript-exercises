4decb2bcba511379ce258765bb6d45a4af66116e
Bryan Chen <bryancgl1198@gmail.com>
2022-10-24 17:39:23 +0800

    Add solutions for 01-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray,element) {    
+    let filterArray = [...arguments].slice(1);
+    const outputArray = inputArray.filter(elem => filterArray.includes(elem) == false);
+    return outputArray;
 };
 
 // Do not edit below this line
